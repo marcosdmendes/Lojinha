69d354f573c1f858413bc5c89effeeaba6202c43
marcos <marcosdmendes@gmail.com>
2016-11-12 00:39:11 -0200

    Produto

---
+++ Lojinha.LojaVirtual.Dominio/Produto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lojinha.LojaVirtual.Dominio
+{
+    public class Produto
+    {
+        public int Produtoid { get; set; }
+        public string Descricao { get; set; }
+        public decimal preco { get; set; }
+        public string categoria { get; set; }
+    }
+}
