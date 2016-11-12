d6c9400794994202e7ddc09a90f5473096a8acad
marcos <marcos@NB-MARCOS>
2016-11-12 00:09:53 -0200

    Add project files.

---
+++ Lojinha.LojaVirtual.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lojinha.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class UnitTestLojinha
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Lojinha.LojaVirtual.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lojinha.LojaVirtual.Web.Startup))]
+namespace Lojinha.LojaVirtual.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
